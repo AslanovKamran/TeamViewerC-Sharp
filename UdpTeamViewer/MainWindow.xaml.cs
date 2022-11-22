706a2f07d4c686afcbf7575c995764a40b680991
Aslan17 <k.aslan1707@gmail.com>
2022-11-22 15:52:17 +0400

    C# Team Viewer Initial Commit

---
+++ UdpTeamViewer/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace UdpTeamViewer
+{
+	//SERVER
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		#region PropertyChanged Realization
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void InvokePropChanged([CallerMemberName] string prop = default)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+
+		#endregion
+
+		//Port And Image
+		#region Bindings
+		private int _port;
+		public int Port
+		{
+			get { return _port; }
+			set { _port = value; InvokePropChanged(); }
+		}
+
+		private ImageSource _image;
+
+		public ImageSource Image
+		{
+			get { return _image; }
+			set { _image = value; InvokePropChanged(); }
+		}
+
+		#endregion
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = this;
+			Port = 8080;
+		}
+		private async void ConnectButon_Click(object sender, RoutedEventArgs e)
+		{
+			await RecieveImage();
+		}
+
+		public Task RecieveImage()
+		{
+			return Task.Run(() =>
+			{
+				try
+				{
+					UdpClient udpClient = new UdpClient(Port); 
+					IPEndPoint endPoint = null;
+
+					using (MemoryStream memory = new MemoryStream())
+					{
+						while (true)
+						{
+							byte[] bytes = udpClient.Receive(ref endPoint);
+							if (bytes.Length == 3 && bytes[0] == 1 && bytes[1] == 2 && bytes[2] == 3)
+							{
+								Application.Current.Dispatcher.Invoke(() =>
+								{
+									Image = BytesToImage(memory.GetBuffer());
+									memory.SetLength(0);
+								});
+							}
+							else
+							{
+								memory.Write(bytes, 0, bytes.Length);
+
+							}
+						}
+
+					}
+
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			});
+		}
+		public ImageSource BytesToImage(byte[] imageData)
+		{
+			try
+			{
+				BitmapImage bitImage = new BitmapImage();
+				MemoryStream ms = new MemoryStream(imageData);
+
+				bitImage.BeginInit();
+				bitImage.StreamSource = ms;
+				bitImage.EndInit();
+
+				ImageSource imgSource = bitImage;
+				return imgSource;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				throw;
+			}
+		}
+	}
+}
---
+++ UdpTeamViewerClient/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UdpTeamViewerClient
+{
+	//CLIENT
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		#region PropertyChanged Realization
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void InvokePropChanged([CallerMemberName] string prop = default)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+
+		#endregion
+		// Ip And Port
+		#region Bindings
+
+		private string _ip;
+		public string Ip
+		{
+			get { return _ip; }
+			set { _ip = value; InvokePropChanged(); }
+		}
+		private int _port;
+		public int Port
+		{
+			get { return _port; }
+			set { _port = value; InvokePropChanged(); }
+		}
+
+		#endregion
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = this;
+			Port = 8080;
+			Ip = "127.0.0.1";
+		}
+		private async void ClientStartButton_Click(object sender, RoutedEventArgs e)
+		{
+			await SendScreen();
+		}
+		public byte[] CaptureScreen()
+		{
+			int width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+			int height = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+			using (MemoryStream memory = new MemoryStream())
+			{
+				Bitmap bmp = new Bitmap(width, height);
+				using (Graphics g = Graphics.FromImage(bmp))
+				{
+					g.CopyFromScreen(0, 0, 0, 0, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size);
+					Bitmap objBitmap = new Bitmap(bmp, (int)(width), (int)(height));
+					objBitmap.Save(memory, ImageFormat.Jpeg);
+				}
+				return memory.GetBuffer();
+			}
+		}
+		public Task SendScreen()
+		{
+			return Task.Run(() =>
+			{
+				UdpClient udpClient = new UdpClient();
+				while (true)
+				{
+					Thread.Sleep(100);
+					byte[] bytes = CaptureScreen();
+					byte[] chunk = new byte[5_000];
+					int bytesCount = 0;
+					using (MemoryStream memory = new MemoryStream(bytes))
+					{
+						while ((bytesCount = memory.Read(chunk, 0, chunk.Length)) != 0)
+						{
+							udpClient.Send(chunk, bytesCount, Ip, Port);
+						}
+					}
+
+					//Token of bytes end
+					udpClient.Send(new byte[3] {1,2,3}, 3, Ip, Port);
+				}
+			});
+		}
+	}
+}
